2b757ec2c271afd45271e3bdfc7ffbd95026aaec
efrenmartinez <developer.emartinez@gmail.com>
2021-08-04 14:14:29 -0500

    :ambulance: Upgrade dependencies
    New version

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import "../assets/styles/App.scss";
-import Logo from "../assets/images/logo-platzi-video-BW2.png";
+import React from 'react'
+import '../assets/styles/App.scss'
+// import Logo from "../assets/images/logo-platzi-video-BW2.png";
 
 const HelloWorld = () => (
-  <div>
-    <h1>Hello World</h1>
-    <img src={Logo} alt="logo" />
-  </div>
-);
+	<div>
+		<h1>Hello World</h1>
+		{/* <img src={Logo} alt="logo" /> */}
+	</div>
+)
 
-export default HelloWorld;
+export default HelloWorld
